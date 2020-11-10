4d8f478af9b8f1b57741cdaecf5562a3014d785e
Callum Asplen <casplen@outlook.com>
2020-11-10 19:55:28 +0000

    Added ability to edit gym classes

--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -38,3 +38,25 @@ def show(id):
     gym_class = gym_class_repository.select(id)
     # members booked goes here
     return render_template("gym_classes/show.html", gym_class = gym_class)
+
+@gym_classes_blueprint.route("/gym_classes/<id>/edit")
+def edit_gym_class(id):
+    gym_class = gym_class_repository.select(id)
+    class_types = gym_class_repository.select_types()
+    return render_template("gym_classes/edit.html", gym_class = gym_class, class_types = class_types)
+
+@gym_classes_blueprint.route("/gym_classes/<id>", methods=['POST'])
+def update_gym_class(id):
+    class_type = request.form["gym_class_type"]
+
+    check = gym_class_repository.check_type_exists(class_type)
+    if check == False:
+        gym_class_repository.new_type(class_type)
+
+    date = request.form["date"]
+    time = request.form["time"]
+    capacity = request.form["capacity"]
+    duration = request.form["duration"]
+    edited_class = GymClass(class_type, date, time, capacity, duration, id)
+    gym_class_repository.update(edited_class)
+    return redirect("/gym_classes")
