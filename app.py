56ec61f4ef5f09fbd562acba5edd1c562ab2bd81
Callum Asplen <casplen@outlook.com>
2020-11-11 15:45:39 +0000

    Added ability to make and view bookings

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 from controllers.gym_class_controller import gym_classes_blueprint
 from controllers.member_controller import member_blueprint
+from controllers.booking_controller import bookings_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(gym_classes_blueprint)
 app.register_blueprint(member_blueprint)
+app.register_blueprint(bookings_blueprint)
 
 @app.route('/')
 def home():
--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -7,3 +7,27 @@ from models.booking import Booking
 import repositories.gym_class_repository as gym_class_repository
 import repositories.member_repository as member_repository
 import repositories.booking_repository as booking_repository
+
+bookings_blueprint = Blueprint("bookings", __name__)
+
+@bookings_blueprint.route("/bookings")
+def bookings():
+    bookings = booking_repository.select_all()
+    return render_template("/bookings/index.html", bookings = bookings)
+
+@bookings_blueprint.route("/bookings/new")
+def new_booking():
+    members = member_repository.select_all()
+    gym_classes = gym_class_repository.select_all()
+    return render_template("/bookings/new.html", members = members, gym_classes = gym_classes)
+
+@bookings_blueprint.route("/bookings", methods=['POST'])
+def add_booking():
+    member_id = request.form["member"]
+    gym_class_id = request.form["gym_class"]
+    booked_gym_class = gym_class_repository.select(gym_class_id)
+    booked_member = member_repository.select(member_id)
+    booking = Booking(booked_member, booked_gym_class)
+    booking_repository.save(booking)
+    return redirect("/bookings")
+
